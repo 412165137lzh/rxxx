12d9818d674c8e940d80be102e6720b6ea2361ae
LZH <412165137@qq.com>
2019-05-08 23:01:26 +0800

    提交test工程到仓库中

---
+++ test/src/com/lzh/Test.java
@@ -0,0 +1,22 @@
+package com.lzh;
+
+public class Test {
+    private String name;
+    void te(){
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println();
+        int[] a = {1,2,3};
+
+        System.out.println(123);
+        System.out.println(123);
+        System.out.println(123);
+        System.out.println(123);
+        System.out.println(123);
+
+    }
+
+
+}
